cb68a38d868a64e9828a2a1f39babb6808c127c3
Айдар Яманаев <iamanaev6@yandex.ru>
2017-03-20 11:23:32 +0200

    Add files via upload (#1)

---
+++ CoffeeMachine/CoffeeMachine/CoffeeMachine.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CoffeeMachine
+{
+    class CoffeeMachine
+    {
+        
+        public Cup CreateCup()
+        {
+            return new Cup();
+        }
+
+        public static void Go()
+        {
+            CoffeeMachine coffeemachine = new CoffeeMachine();
+
+            foreach (string s in Program.Interface.machine)
+            {
+                Console.WriteLine(s);
+            }
+
+            int count = 0;
+            ConsoleKeyInfo key = new ConsoleKeyInfo();
+            do
+            {
+                Program.Interface.ChangeOrder();
+                switch (count)
+                {
+                    case 0:
+                        Console.Write("espresso - 30   ");
+                        break;
+                    case 1:
+                        Console.Write("americano - 40  ");
+                        break;
+                    case 2:
+                        Console.Write("capuccino - 42.5");
+                        break;
+                    case 3:
+                        Console.Write("tea - 15        ");
+                        break;
+                }
+
+                key = Console.ReadKey();
+                if (key.Key.Equals(ConsoleKey.LeftArrow)) count--;
+                if (key.Key.Equals(ConsoleKey.RightArrow)) count++;
+                if (count > 3) count = 0;
+                if (count < 0) count = 3;
+
+            }
+            while (!key.Key.Equals(ConsoleKey.Enter));
+
+
+            Program.Interface.ChangeOrder();
+            Liquid l;
+            switch (count)
+            {
+                case 0:
+                    l = new Espresso();
+                    
+                    break;
+                case 1:
+                    l = new Americano();
+                    
+                    break;
+                case 2:
+                    l = new Capuccino();
+                    
+                    break;
+                case 3:
+                    l = new Tea();
+                    
+                    break;
+                default:
+                    l = new Espresso();
+                    break;
+            }
+
+
+            Program.Interface.ChangeOrderText();
+            Console.Write("        Do you want sugar?        ");
+            count = 0;
+            do
+            {
+                Program.Interface.ChangeOrder();
+                switch (count)
+                {
+                    case 0:
+                        Console.Write("Yes - +5");
+                        break;
+                    case 1:
+                        Console.Write("No      ");
+                        break;
+                }
+                key = Console.ReadKey();
+                if (key.Key.Equals(ConsoleKey.LeftArrow)) count--;
+                if (key.Key.Equals(ConsoleKey.RightArrow)) count++;
+                if (count < 0) count = 1;
+                if (count > 1) count = 0;
+            }
+            while (!key.Key.Equals(ConsoleKey.Enter));
+
+            switch (count)
+            {
+                case 0:
+                    l.HasSugar = true;
+                    l.price += 5;
+                    break;
+                case 1:
+                    l.HasSugar = false;
+                    break;
+            }
+            Console.SetCursorPosition(10, 10);
+            Console.Write($"Your order costs {l.price}");
+            Program.Interface.SetMoney();
+            string s1="";
+            while (!int.TryParse(s1, out int n))
+            {
+                s1 = Console.ReadLine();
+                Program.Interface.SetMoney();
+            }
+            if (Program.Interface.CalculateChange(l, double.Parse(s1)))
+            {
+                Program.Interface.ChangeOrder();
+                Program.Interface.ChangeOrderText();
+                Console.Write("     Take your order, please     ");
+
+                Program.Interface.ChangeFinalText();
+                coffeemachine.CreateCup().Fill(l);
+                Program.Interface.ReturnOrder();
+                Console.SetCursorPosition(0, 19);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.SetCursorPosition(0, 19);
+                Console.ReadKey();
+            }
+
+        }
+    }
+    
+
+    abstract class Liquid
+    {
+        public bool HasSugar;
+        public string info;
+        public double price;
+    }
+
+    class Espresso: Liquid
+    { 
+        public Espresso()
+        {
+            info = "espresso";
+            price = 30;
+        }
+    }
+
+    class Americano : Liquid
+    {
+        public Americano()
+        {
+            info = "americano";
+            price = 40;
+        }
+    }
+
+    class Capuccino : Liquid
+    {
+        public Capuccino()
+        {
+            info = "capuccino";
+            price = 42.5;
+        }
+    }
+
+    class Tea : Liquid
+    {
+        public Tea()
+        {
+            info = "tea";
+            price = 15;
+        }
+    }
+
+    class Cup
+    {
+        public void Fill(Liquid l)
+        {
+            if (l.HasSugar)
+                Console.Write($"Your {l.info} with sugar");
+            else Console.WriteLine($"Your {l.info} without sugar");
+        }
+    }
+}
---
+++ CoffeeMachine/CoffeeMachine/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace CoffeeMachine
+{
+    class Program
+    {
+        public class Interface
+        {
+            public static string[] machine = File.ReadAllLines("Machine.txt");
+
+            public static void ChangeOrder()
+            {
+                Console.SetCursorPosition(4, 6);
+                Console.Write("                   ");
+                Console.SetCursorPosition(4, 6);
+            }
+
+            public static void SetMoney()
+            {
+                
+                Console.SetCursorPosition(12, 12);
+                Console.Write("           ");
+                Console.SetCursorPosition(12, 12);
+            }
+
+            public static bool CalculateChange(Liquid l, double c)
+            {
+                Console.SetCursorPosition(10, 10);
+                Console.Write("                         ");
+                Console.SetCursorPosition(10, 10);
+
+                if (c >= l.price)
+                {
+                    Console.Write("Take your exchange");
+                    SetMoney();
+                    Console.Write((c - l.price).ToString());
+                    return true;
+                }
+                else { Console.Write("Insufficient funds"); }
+                return false;
+            }
+
+            public static void ReturnOrder()
+            {
+                Console.SetCursorPosition(36, 12);
+                Console.Write(" |________| ");
+                Console.SetCursorPosition(36, 13);
+                Console.Write(" |        | ");
+                Console.SetCursorPosition(36, 14);
+                Console.Write("  \\      /   ");
+                Console.SetCursorPosition(36, 15);
+                Console.Write("   \\____/   ");
+                Console.SetCursorPosition(36, 16);
+                Console.Write("___/____\\___");
+            }
+
+            public static void ChangeOrderText()
+            {
+                Console.SetCursorPosition(1, 4);
+                Console.Write("                                  ");
+                Console.SetCursorPosition(1, 3);
+                Console.Write("                                  ");
+                Console.SetCursorPosition(1, 4);
+            }
+
+            public static void ChangeFinalText()
+            {
+                Console.SetCursorPosition(1, 16);
+                Console.Write("                              ");
+                Console.SetCursorPosition(2, 16);
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            
+            CoffeeMachine.Go();
+        }
+    }
+}
